f8440dddfb6c5eda19469289bff6b84ec315161b
harirkp006 <harirkp006@gmail.com>
2026-01-17 13:40:45 +0530

    Update long.py

--- long.py
+++ long.py
@@ -1,1 +1,17 @@
-longestpalindrome
+def longestpalindrome(s:str)->int:
+    freq={}
+    for i in s:
+        freq[i]=freq.get(i,0)+1
+        length=0
+        odd=False
+        for count in freq.values():
+            if count%2==0:
+                length+=count
+            else:
+                length=count-1
+                odd=True
+        if odd:
+            length+=1
+    return length
+s=input()
+print(longestpalindrome(s))
